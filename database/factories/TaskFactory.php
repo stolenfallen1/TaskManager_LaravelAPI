dcdda8a2315af6eefd0c883f74f9d84dfb19a978
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-10-05 20:20:26 +0800

    user model

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -20,6 +21,7 @@ class TaskFactory extends Factory
             //
             'title' => $this->faker->sentence(),
             'is_done' => false,
+            'creator_id' => User::factory(),
         ];
     }
 }
