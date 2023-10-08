418ca1a127cd3a0758c98983397ea61045d36341
Paw <76894433+stolenfallen1@users.noreply.github.com>
2023-10-08 11:24:28 +0800

    fetch project list and add include params

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use App\Http\Resources\ProjectCollection;
 use App\Http\Resources\ProjectResource;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ProjectController extends Controller
 {
     //
+    // Index all projects
+    public function index(Request $request)
+    {
+        $projects = QueryBuilder::for(Project::class)
+            ->allowedIncludes('tasks')
+            ->paginate();
+        return new ProjectCollection($projects);
+    }
 
     // Store / Create a new project
     public function store(StoreProjectRequest $request)
@@ -26,7 +36,7 @@ class ProjectController extends Controller
     // Show a project
     public function show(Request $request, Project $project)
     {
-        return new ProjectResource($project);
+        return (new ProjectResource($project))->load('tasks');
     }
 
     // Update a project
